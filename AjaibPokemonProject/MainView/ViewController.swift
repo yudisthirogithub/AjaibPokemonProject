42863ad5569b1aa112bc7bf1c9ff269bbd79da59
yudisthirogithub <yudis_deon@yahoo.com>
2021-10-21 15:33:49 +0700

    testing API data to local storage

--- AjaibPokemonProject/MainView/ViewController.swift
+++ AjaibPokemonProject/MainView/ViewController.swift
@@ -12,6 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var pokemonCollectionView: UICollectionView!
     
     private let spacing:CGFloat = 8.0
+    private let Backgroundcolor = UIColor(hexString: "#1A202C")
+    private let navBackgroundColor = UIColor(hexString: "#161B22")
+    
+    
+    
+    private var navBar : UINavigationBar = {
+        let navbar = UINavigationBar()
+        navbar.translatesAutoresizingMaskIntoConstraints = false
+        
+        return navbar
+    }()
+    
+    private var searchPokemonBar : UISearchBar = {
+        let searchpokemonbar = UISearchBar()
+        searchpokemonbar.translatesAutoresizingMaskIntoConstraints = false
+        
+        return searchpokemonbar
+    }()
     
     //dummy data
     private var items = ["007","jungle","shangchi"]
@@ -20,19 +38,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = Backgroundcolor
+        navBar.backgroundColor = navBackgroundColor
+        
+        view.addSubview(navBar)
+        
+        
+        
         pokemonCollectionView.register(PokemonCollectionViewCell.nib(), forCellWithReuseIdentifier: PokemonCollectionViewCell.cellName())
+        
+
         
         pokemonCollectionView.dataSource = self
         pokemonCollectionView.delegate = self
-        //        pokemonCollectionView.sizeToFit()
-        //        pokemonCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 17)
-        
-        
+    
         //API Stuffs
         ServiceAPI.shared.getResults(pokemonName: "Pikachu") { result in
             switch result {
             case .success(let results):
-                print(results)
+//                print(results)
+                
+                var pokemons = Model()
+                
+                for i in 0...5{
+                    pokemons.name = results.data[i].name
+                    pokemons.types = results.data[i].types
+                    pokemons.subtypes = results.data[i].subtypes
+  
+                    print("\(pokemons.name) + \(pokemons.types) + \(pokemons.subtypes)")
+                    
+                }
                 
             case .failure(let error):
                 print(error)
@@ -40,36 +75,43 @@ class ViewController: UIViewController {
         }
         
         addConstraints()
+        pokemonCollectionView.backgroundColor = .clear
     }
     
     
     private func addConstraints() {
         
+        self.navBar.snp.makeConstraints{
+            make in
+            make.width.equalToSuperview()
+            make.height.equalTo(100)
+        }
+        
+//        self.searchPokemonBar.snp.makeConstraints{
+//            make in
+////            make.leading.equalTo(16)
+////            make.trailing.equalTo(-16)
+//            make.top.equalTo(self.navBar.snp.height).offset(10)
+//
+//        }
+        
         self.pokemonCollectionView.snp.makeConstraints{ make in
-            //            make.trailing.top.width.equalToSuperview()
-            //            make.width.top.equalToSuperview()
-            //            make.height.equalToSuperview()
-            //            make.width.equalToSuperview()
-            //            make.leadingMargin.equalTo(16)
-            //            make.trailingMargin.equalTo(16)
-            //            make.topMargin.equalTo(10)
-            //            make.bottomMargin.equalTo(10)
-            
+//            make.leading.equalTo(16)
+//            make.trailing.equalTo(-16)
+//            make.top.equalTo(10)
+//            make.bottom.equalTo(-10)
             
             make.leading.equalTo(16)
             make.trailing.equalTo(-16)
-            make.top.equalTo(10)
+            make.top.equalTo(108)
             make.bottom.equalTo(-10)
             
-            
-            //            make.leading.equalToSuperview().offset(16)
-            //            make.trailing.equalToSuperview().offset(-16)
-            //            make.top.equalTo(10)
-            //            make.bottom.equalTo(-10)
+
             
         }
     }
 }
+
 
 // MARK: - Collection View Delegate
 extension ViewController: UICollectionViewDelegate {}
@@ -102,5 +144,35 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
         }else{
             return CGSize(width: 0, height: 0)
         }
+    }
+}
+
+// MARK: - Extension for UIColor using HEX Code
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
     }
 }
---
+++ AjaibPokemonProject/Model/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  AjaibPokemonProject
+//
+//  Created by Yudis Huang on 21/10/21.
+//
+
+import Foundation
+
+class Model {
+    var name : String?
+    var subtypes : [String]?
+    var types : [String]?
+    var flavorText : String?
+    var images : String?
+    
+    required init() {}
+}
+
+
